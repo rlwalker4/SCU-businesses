aeb02189e3b1ef90b90589fa31c8a2bf42446e88
Liam Walker <rlwalker@scu.edu>
2018-10-31 18:33:23 -0700

    Style Sheet

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <HTML>
 <HEAD>
 <TITLE>SCU Businesses</TITLE>
@@ -17,6 +18,13 @@ Find SCU alumni-owned businesses. If you are an SCU Alum, please add your busine
 </UL>
 
 <HR COLOR="#990033">
+
+<ul class="w3-ul w3-border">
+    <li><h2>Names</h2></li>
+    <li>Jill</li>
+    <li>Eve</li>
+    <li>Adam</li>
+</ul>
 
 <br>
 <?php
