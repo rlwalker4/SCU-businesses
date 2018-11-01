d407f24564aec1c37086726fd02e911f6f38e52c
Stephen Poth <sspoth@gmail.com>
2018-11-01 13:53:47 -0700

    Update index.php

--- index.php
+++ index.php
@@ -12,29 +12,35 @@
 
 
 <div class="w3-row-padding w3-content" style="max-width:1400px">
-<table class="w3-twothird w3-table-all w3-card-2">
-<tr>
-<th>Name</th>
-<th>Business Type</th>
-<th>Location</th>
-</tr>
-<tr>
-<td>Bob's Burgers</td>
-<td>
-<?php
-    echo "PHP";
-    ?>
-</td>
-<td>SCU</td>
-</tr>
-</table>
+    <table class="w3-twothird w3-table-all w3-card-2">
+    <tr>
+    <th>Name</th>
+    <th>Business Type</th>
+    <th>Location</th>
+    </tr>
+    <tr>
+    <td>Bob's Burgers</td>
+    <td>
+    <?php
+        echo "PHP";
+        ?>
+    </td>
+    <td>SCU</td>
+    </tr>
+    </table>
 
-<div class="w3-third">
-<div class="w3-container w3-light-grey w3-justify">
-<h3>Filter By: </h3>
+    <div class="w3-third">
+        <div class="w3-container w3-light-grey w3-justify">
+            <h3>Filter By: </h3>
+        </div>
+        <br>
+        <div class="w3-container w3-light-grey w3-justify">
+            <h3>Add New Business: </h3>
+        </div>
+
+    </div>
 </div>
-</div>
-</div>
+
 
 
 <br>
