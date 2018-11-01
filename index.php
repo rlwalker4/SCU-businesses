c964e0f3c4322cbf4e9bf8163205985123737b64
Liam Walker <rlwalker@scu.edu>
2018-10-31 18:08:14 -0700

    removed weird git stuff

--- index.php
+++ index.php
@@ -14,15 +14,12 @@ Find SCU alumni-owned businesses. If you are an SCU Alum, please add your busine
 <HR COLOR="#990033">
 
 <UL>
-<<<<<<< HEAD
   <LI><A HREF="http://www.scu.edu">Santa Clara University</A>
   <LI><A HREF="http://www.engr.scu.edu">SCU School of Engineering</A>
   <LI><A HREF="http://www.dc.engr.scu.edu">SCU Engineering Design Center</A>
   <LI><A HREF="http://students.engr.scu.edu">Engineering Student Webpages</A>
   <LI><A HREF="AlumniOffice.php">Test page</A>
-=======
 <LI><A HREF="test.php">Alumni Office Use Only</A>
->>>>>>> 0dd00f92e1f0778244b14877ca953b1279047525
 </UL>
 
 <HR COLOR="#990033">
