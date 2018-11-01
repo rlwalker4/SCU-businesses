1e3a6ea7bf6044997d1f84a416228e5088459983
Stephen Poth <sspoth@gmail.com>
2018-10-31 19:38:01 -0700

    Update index.php

--- index.php
+++ index.php
@@ -2,24 +2,30 @@
 <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-colors-flat.css">
 <HTML>
 <HEAD>
-<TITLE>SCU Businesses</TITLE>
+    <TITLE>SCU Businesses Directory</TITLE>
 </HEAD>
 
 <div class="w3-flat-pomegranate">
-<div Align =left> <a href="AlumniOffice.php" class="w3-button w3-black">Alumni Office Login</a></div>
-<H1 ALIGN=CENTER>SCU Alumni Businesses</H1>
-
-
+    <div Align =left> <a href="AlumniOffice.php" class="w3-button w3-black">Alumni Office Login</a></div>
+    <H1 ALIGN=CENTER>SCU Alumni Businesses</H1>
 </div>
 
-</BODY>
 
-<ul class="w3-ul w3-border">
-    <li><h2>Names</h2></li>
-    <li>Jill</li>
-    <li>Eve</li>
-    <li>Adam</li>
-</ul>
+<div class="w3-row-padding w3-content" style="max-width:1400px">
+    <div class="w3-twothird">
+        <ul class="w3-ul w3-border">
+            <li><h2>Names</h2></li>
+            <li>Jill</li>
+            <li>Eve</li>
+            <li>Adam</li>
+        </ul>
+    </div>
+    <div class="w3-third">
+        <div class="w3-container w3-light-grey w3-justify">
+            <h2>Very New News!</h2>
+        </div>
+    </div>
+</div>
 
 <br>
 
