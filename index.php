d561404e94d1d6c1ec7aec0417f7f4fb87e639a6
Robert Liam Walker <rwalker@linux60408.dc.engr.scu.edu>
2018-11-08 16:06:35 -0800

    right to left

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-colors-flat.css">
+<link href="https://fonts.googleapis.com/css?family=Lato:300,700|Prata" rel="stylesheet">
 <HTML>
 <HEAD>
 <TITLE>SCU Businesses</TITLE>
+<style>
+h1{
+	font-family: 'Prata', serif;
+	color: #FFFFFF;
+	background-color: #72001B;
+}
+h3{
+	color: #72001B;
+}
+</style>
 </HEAD>
 
 <?php
@@ -17,8 +28,6 @@
 	}
 ?>
 <button onclick="document.getElementById('id01').style.display='block'" class="w3-button w3-black">Alumni Office Login</button>
-
-
     <div id="id01" class="w3-modal">
         <div class="w3-modal-content w3-animate-top w3-card-4">
             <header class="w3-container w3-flat-pomegranate">
@@ -34,12 +43,37 @@
     </div>
 
 <div Align =left> <a href="AlumniOffice.php" class="w3-button w3-black">Alumni Office Login</a></div>
-<H1 ALIGN=CENTER>SCU Alumni Businesses</H1>
 
+<div class="w3-container">
+	<h1 ALIGN=CENTER>SCU Alumni Businesses
+	</h1>
+</div>
 
-<div class="w3-row-padding w3-content" style="max-width:1400px">
-	
-
+<div class="w3-row-padding w3-content">
+    <div class="w3-third">
+        <div class="w3-container w3-light-grey w3-justify">
+            <h3>Filter By</h3>
+                <form action="index.php" method = "post">
+                Location<br><input type="text" name="LocationF" value=""><br>
+                Business Type<br><input type="text" name="TypeF" value=""><br>
+                <input type="submit" value="Filter">
+                </form>
+        </div>
+        <br>
+        <div class="w3-container w3-light-grey w3-justify">
+            <h3>Add New Business</h3>
+                <form action="index.php" method = "post">
+                Name<br> <input type="text" name="Name" value=""><br>
+                Location<br><input type="text" name="LocationA" value=""><br>
+                Type<br><input type="text" name="TypeA" value=""><br>
+                Info<br><input type="text" name="Info" value=""><br>
+                Grad Year<br><input type="text" name="GradYear" value=""><br>
+                Username<br><input type="text" name="Username" value=""><br>
+                Degree<br><input type="text" name ="Degree" value = ""><br>
+                <input type="submit" value="Submit">
+                </form>
+        </div>
+    </div>
 	<?php
 		$i = 0;
 		foreach ($_POST as $key => $var){
@@ -57,31 +91,6 @@
 		
 		//getListings(1);
 	?>
-
-    <div class="w3-third">
-        <div class="w3-container w3-light-grey w3-justify">
-            <h3>Filter By: </h3>
-                <form action="index.php" method = "post">
-                Location<br><input type="text" name="LocationF" value=""><br>
-                Business Type<br><input type="text" name="TypeF" value=""><br>
-                <input type="submit" value="Filter">
-                </form>
-        </div>
-        <br>
-        <div class="w3-container w3-light-grey w3-justify">
-            <h3>Add New Business: </h3>
-                <form action="index.php" method = "post">
-                Name<br> <input type="text" name="Name" value=""><br>
-                Location<br><input type="text" name="LocationA" value=""><br>
-                Type<br><input type="text" name="TypeA" value=""><br>
-                Info<br><input type="text" name="Info" value=""><br>
-                Grad Year<br><input type="text" name="GradYear" value=""><br>
-                Username<br><input type="text" name="Username" value=""><br>
-                Degree<br><input type="text" name ="Degree" value = ""><br>
-                <input type="submit" value="Submit">
-                </form>
-        </div>
-    </div>
 </div>
 
 <p>
